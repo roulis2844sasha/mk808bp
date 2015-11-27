aeaff56aa1e5c157b68c0cce3f59243a564e539b
Alexander Roulisov <roulis2844sasha@gmail.com>
2015-11-27 19:21:22 +0100

    add lib

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cp 8723bs.ko /lib/modules/3.10.70/kernel/drivers/net/wireless/
+sudo depmod -a
+sudo modprobe 8723bs
+sudo sed -i -e '$d' /etc/rc.local
+sudo sh -c 'echo "modprobe 8723bs\nexit 0" >> /etc/rc.local'
+sudo passwd minipc
